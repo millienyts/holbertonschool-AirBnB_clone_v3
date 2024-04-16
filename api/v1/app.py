637aaed72d4bde0f610e53002a5a77d0bea6926f
millienytorressanta <tmillieny@gmail.com>
2024-04-16 15:00:04 -0400

    adding files with changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,33 +4,34 @@
 Contains TestFileStorageDocs classes for documentation and style checks.
 """
 
-from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+from os import environ
+from flask import Flask, jsonify, make_response
 from flask_cors import CORS
 
+
 app = Flask(__name__)
+
 app.register_blueprint(app_views)
-cors = CORS(app, resources="/*", origins="0.0.0.0")
+
+
+# Create a CORS instance with permissive settings (for demonstration purposes)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """ This module teardown connection to db"""
+def close_db(exception):
+    """Close storage"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found_json_output(exception):
-    """
-    Returns JSON {'error': 'Not found'}, 404
-    when a 404 error occurs
-    """
-    return {'error': 'Not found'}, 404
+def not_found(exception):
+    """404 error"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', '5000')
-    app.run(host=host, port=port, threaded=True)
+    """Main"""
+    app.run(host='0.0.0.0', port=5000)

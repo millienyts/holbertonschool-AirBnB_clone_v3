510e56fdce6e2fed9b9248dec4aac466d53563ba
millienytorressanta <tmillieny@gmail.com>
2024-04-18 02:45:29 -0400

    api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,7 +3,6 @@
 
 
 from api.v1.views.index import *
-from api.v1.views.index import app_views
 from api.v1.views.states import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *

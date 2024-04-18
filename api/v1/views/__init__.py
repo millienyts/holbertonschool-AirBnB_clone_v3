857018b91d321b336da1fe6ba468b2fae743ad85
millienytorressanta <tmillieny@gmail.com>
2024-04-18 03:04:35 -0400

    api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,14 +1,5 @@
-#!/usr/bin/python3
-"""Script for Blueprint for flask"""
-
-
+"""Initialize Flask Blueprint object."""
 from flask import Blueprint
-from api.v1.views.index import app_views
-from api.v1.views.states import app_views as states_views
-from api.v1.views.places import app_views as places_views
-from api.v1.views.places_reviews import app_views as places_reviews_views
-from api.v1.views.cities import app_views as cities_views
-from api.v1.views.amenities import app_views as amenities_views
-from api.v1.views.users import app_views as users_views
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,45 +1,9 @@
-#!/usr/bin/python3
-"""
-API Status Page
+"""Index module for Flask app."""
 
-Contains routes for the API status page, showing the status (OK or not) and the count
-of objects of each type in 'storage.all()'.
+from api.v1.views import app_views
+from flask import jsonify
 
-"""
-from flask import Blueprint, jsonify
-from models import storage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-
-app_views = Blueprint('app_views', __name__)
-
-
-@app_views.route("/status")
-def check_status():
-    """ return status ok as json"""
-    dict_ = {'status': "OK"}
-
-    return jsonify(dict_)
-
-
-@app_views.route('/stats')
-def model_statistics():
-    """
-    Returns the counts of all the
-    objects in 'storage.all()',
-    counted by each type
-    """
-    return {
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User)
-    }
-    return jsonify(stats)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def api_status():
+    """Endpoint that returns the status of the API."""
+    return jsonify({"status": "OK"})

bcee84dd1e2d9634bb5084ec7d57bb4f41ed817a
millienytorressanta <tmillieny@gmail.com>
2024-04-18 02:50:40 -0400

    api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,7 +3,7 @@
 
 
 from flask import Blueprint
-from api.v1.views.index import app_views as index_views
+from api.v1.views.index import app_views
 from api.v1.views.states import app_views as states_views
 from api.v1.views.places import app_views as places_views
 from api.v1.views.places_reviews import app_views as places_reviews_views

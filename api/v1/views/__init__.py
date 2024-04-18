08452d27adf6a58f234f0936c38305f1bd60a6c5
millienytorressanta <tmillieny@gmail.com>
2024-04-18 02:49:12 -0400

    api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,13 +2,13 @@
 """Script for Blueprint for flask"""
 
 
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
 from flask import Blueprint
+from api.v1.views.index import app_views as index_views
+from api.v1.views.states import app_views as states_views
+from api.v1.views.places import app_views as places_views
+from api.v1.views.places_reviews import app_views as places_reviews_views
+from api.v1.views.cities import app_views as cities_views
+from api.v1.views.amenities import app_views as amenities_views
+from api.v1.views.users import app_views as users_views
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')

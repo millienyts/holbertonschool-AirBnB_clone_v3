14e39c94588e3f0abec770128a6925c841a2ee3d
millienytorressanta <tmillieny@gmail.com>
2024-04-18 03:16:23 -0400

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,22 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def api_status():
     """Endpoint that returns the status of the API."""
     return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Endpoint to retrieve stats of objects by type."""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)

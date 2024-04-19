362b0541ca20e483a5d9b75c74dc65afd6f1e8b4
Ltoro9 <louistororosario@gmail.com>
2024-04-18 21:03:12 -0400

    pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 """Main module of the Flask app."""
+
+
 from flask import Flask
 from os import getenv
 from api.v1.views import app_views
 
+
 app = Flask(__name__)
 host = getenv('HBNB_API_HOST', '0.0.0.0')
 port = getenv('HBNB_API_PORT', '5000')
+
 
 @app.teardown_appcontext
 def close_storage(exception):
     """Close the storage on teardown."""
     from models import storage
     storage.close()
+
 
 if __name__ == '__main__':
     app.register_blueprint(app_views)

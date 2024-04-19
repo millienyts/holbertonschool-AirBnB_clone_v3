1a170a3eeabd7803331f499a1b9e2681c72e6dd4
Ltoro9 <louistororosario@gmail.com>
2024-04-18 22:46:30 -0400

    route fixed, it was duplaicate /api/v1

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -13,7 +19,7 @@ def api_status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
     """Endpoint to retrieve stats of objects by type."""
     stats = {

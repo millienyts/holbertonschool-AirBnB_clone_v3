75c8cae6bdc6c9b4071345226a068c307b7553d4
millienytorressanta <tmillieny@gmail.com>
2024-04-19 01:56:05 -0400

    unnitest

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,51 +1,66 @@
 #!/usr/bin/python3
-from os import getenv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+import json
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
-class DBStorage:
-    """DBStorage class"""
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
-    __engine = None
-    __session = None
 
-    def __init__(self):
-        """constructor"""
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'
-                                      .format(getenv('HBNB_MYSQL_USER'),
-                                              getenv('HBNB_MYSQL_PWD'),
-                                              getenv('HBNB_MYSQL_HOST'),
-                                              getenv('HBNB_MYSQL_DB')))
+class FileStorage:
+    """Serializes instances to a JSON file and deserializes JSON file to instances"""
 
-    def all(self, cls=None):
-        """query on the current database session"""
-        from models.base_model import BaseModel, Base
-        if cls:
-            objects = self.__session.query(cls).all()
-        else:
-            objects = self.__session.query(BaseModel).all()
-        return {"{}.{}".format(type(obj).__name__, obj.id): obj for obj in objects}
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return self.__objects
 
     def new(self, obj):
-        """add object to current database session"""
-        self.__session.add(obj)
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        """commit all changes of the current database session"""
-        self.__session.commit()
-
-    def delete(self, obj=None):
-        """delete from the current database session"""
-        if obj:
-            self.__session.delete(obj)
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        with open(self.__file_path, mode="w", encoding="utf-8") as f:
+            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
 
     def reload(self):
-        """create all tables in the database"""
-        Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
+        """Deserializes the JSON file to __objects (only if the JSON file exists;
+        otherwise, do nothing)."""
+        try:
+            with open(self.__file_path, mode="r", encoding="utf-8") as f:
+                self.__objects = {k: BaseModel(**v) for k, v in json.load(f).items()}
+        except FileNotFoundError:
+            pass
+
+    def delete(self, obj=None):
+        """Delete object"""
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
+
+    def get(self, cls, id):
+        """Retrieve one object"""
+        if cls and id:
+            key = "{}.{}".format(cls.__name__, id)
+            return self.__objects.get(key)
+
+    def count(self, cls=None):
+        """Count number of objects in storage"""
+        if cls:
+            return sum(1 for obj in self.__objects.values() if isinstance(obj, cls))
+        else:
+            return len(self.__objects)
 
     def close(self):
-        """close"""
-        self.__session.remove()
+        """Call reload() method for deserializing the JSON file to objects"""
+        self.reload()
